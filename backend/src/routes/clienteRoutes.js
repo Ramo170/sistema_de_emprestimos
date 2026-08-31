6485f770b427f996fb4df9c30d140b9159e3cb86
Ramo170 <icaro.editor170@gmail.com>
2026-08-31 11:01:17 -0300

    feat: aletrando clientesRouter

--- backend/src/routes/clienteRoutes.js
+++ backend/src/routes/clienteRoutes.js
@@ -3,11 +3,11 @@ const clienteController = require('../controllers/clienteController')
 
 const router = Router();
 
-router.post('/cliente_emprestimos', clienteController.analisarEmprestimos);
-router.post('/clientes', clienteController.criar);
-router.get('/clientes', clienteController.listarTodos);
-router.get('/clientes/:id', clienteController.buscarPorId);
-router.put('/clientes/:id', clienteController.atualizar);
-router.delete('/clientes/:id', clienteController.deletar);
+router.post('/customer-loans', clienteController.analisarEmprestimos);
+router.post('/customers', clienteController.criar);
+router.get('/customers', clienteController.listarTodos);
+router.get('/customers/:id', clienteController.buscarPorId);
+router.put('/customers/:id', clienteController.atualizar);
+router.delete('/customers/:id', clienteController.deletar);
 
 module.exports = router;
