cf8a42adf515d29d95ce6b5fbd94c77e0009227d
Ramo170 <icaro.editor170@gmail.com>
2026-08-31 08:27:42 -0300

    feat: estrutura completa do sistema com frontend e backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require("./routes/clienteRoutes")
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes)
+app.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = app;
