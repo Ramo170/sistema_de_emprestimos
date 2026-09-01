14c8361a120b459c7e2f7fa58fd49c7aefdd7b34
Ramo170 <icaro.editor170@gmail.com>
2026-09-01 08:57:37 -0300

    alterando o database.js

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,15 +2,23 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 
+// TESTE DAS VARIÁVEIS DO RENDER
+console.log('DB_HOST:', process.env.db_host);
+console.log('DB_PORT:', process.env.db_port);
+console.log('DB_USER:', process.env.db_user);
+console.log('DB_NAME:', process.env.db_name);
+
 const pool = mysql.createPool({
     host: process.env.db_host,
     port: Number(process.env.db_port),
     user: process.env.db_user,
     password: process.env.db_password,
     database: process.env.db_name,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+
     ssl: {
         rejectUnauthorized: false
     }
